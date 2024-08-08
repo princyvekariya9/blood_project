e26da76fa999ded2b23ad6d4332c4479089bb1ed
princyvekariya9 <princyvekariya9@gmail.com>
2024-08-08 21:24:45 +0530

    header change

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <!-- Page Title -->
-  <title>Blad Ai - Blood Donation</title>
+  <title>Blad Cloud - Blood Donation</title>
 
   <!-- Favicon Icon -->
   <link rel="shortcut icon" href="assets/images/favicon.php">
@@ -50,19 +50,19 @@
 
   <!-- header start -->
   <header>
-    <div class="header_top d-none d-lg-block d-xl-block d-xxl-block">
+    <!-- <div class="header_top d-none d-lg-block d-xl-block d-xxl-block">
       <div class="container">
         <div class="row">
           <div class="col-xl-3 col-lg-3">
             <div class="header_top_content">
               <span><i class="fa-solid fa-phone"></i></span>
-              <a href="tel:01977259912">+01 (977) 2599 12</a>
+              <a href="tel:917048531203">+91 70485 31203</a>
             </div>
           </div>
           <div class="col-xl-3 col-lg-3">
             <div class="header_top_content">
               <span><i class="fa-solid fa-envelope"></i></span>
-              <a href="mailto:company@domin.com">company@domin.com</a>
+              <a href="mailto:company@domin.com">blood@gmil.com</a>
             </div>
           </div>
           <div class="col-xl-3 col-lg-3">
@@ -84,7 +84,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="header_bottom">
       <div class="container">
         <div class="row align-items-center position-relative">
@@ -93,7 +93,7 @@
               <a href="index.php"><img src="assets/images/logo.png" alt="images not founds" class="img-fluid"></a>
             </div>
           </div>
-          <div class="col-xl-8 col-lg-8 d-none d-xxl-block d-xl-block">
+          <div class="col-xl-10 col-lg-10 d-none d-xxl-block d-xl-block">
             <ul class="main_menu">
               <li class="position-relative">
                 <a href="index.php">Home </a>
@@ -107,119 +107,24 @@
                   <li><a href="campaign-details.php">Campaign Details</a></li>
                 </ul>
               </li>
-               <li>
+               <li class="position-relative">
                 <a href="javascript:void(0)">Pages <i class="fa-solid fa-angle-down"></i></a>
-                <div class="bi_megamenu">
-                  <div class="megamenu_row">
-                    <div class="megamenu-column border-end">
-                      <h6 class="column-title">Home</h6>
-                      <ul class="pages_list">
-                        <li><a href="index-2.php">Home 1</a></li>
-                        <li><a href="index-3.php">Home 2</a></li>
-                        <li><a href="index-4.php">Home 3</a></li>
-                      </ul>
-                    </div>
-                    <div class="megamenu-column border-end">
-                      <h6 class="column-title">Pages</h6>
-                      <ul class="pages_list">
-                        <li><a href="about.php">About Us</a></li>
-                        <li><a href="service.php">Service</a></li>
-                        <li><a href="service-details.php">Service Details</a></li>
-                        <li><a href="team-member.php">Team Member</a></li>
-                        <li><a href="terms.php">Terms & Condation</a></li>
-                        <li><a href="privacypolicy.php">Privacy Policy</a></li>
-                      </ul>
-                    </div>
-                    <div class="megamenu-column border-end">
-                      <h6 class="column-title">Pages</h6>
-                      <ul class="pages_list">
-                        <li><a href="photo-gallary.php">photo-gallary</a></li>
-                        <li><a href="faq.php">faq</a></li>
-                        <li><a href="campaign.php">Campaign</a></li>
-                        <li><a href="campaign-details.php">Campaign Details</a></li>
+                <ul class="submenu_wrapper">
+                  <li><a href="photo-gallary.php">photo-gallary</a></li>
                         <li><a href="donate.php">Donate</a></li>
                         <li><a href="register.php">Register</a></li>
-                        <li><a href="register.php">Contract</a></li>
-                        <li><a href="404.php">404</a></li>
-                      </ul>
-                    </div>
-                    <div class="megamenu-column border-end">
-                      <h6 class="column-title">Blog</h6>
-                      <ul class="pages_list">
-                        <li><a href="blog-default.php">Blog Default</a></li>
-                        <li><a href="blog-details.php">Blog Details</a></li>
-                        <li><a href="blog-style1.php">Blog Style 1</a></li>
-                        <li><a href="blog-style2.php">Blog style 2</a></li>
-                        <li><a href="blog-details-left-sidebar.php">Blog Leftsidebar</a></li>
-                        <li><a href="blog-details-right-sidebar.php">Blog Rightsidebar</a></li>
-                      </ul>
-                    </div>
-                    <div class="megamenu-column banner">
-                      <div class="megamenu-banner img">
-                        <div class="row ">
-                          <div class="col-12">
-                            <div class="fblog_img img">
-                              <a href="blog-details.php"><img src="assets/images/f1.jpg" class="w-100" alt=""></a>
-                            </div>
-                          </div>
-                          <div class="col-12">
-                            <div class="fblog_content">
-                              <a href="blog-details.php"> A Formula For Help And Happiness</a>
-                              <span>18 February, 2022</span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li> 
-              <li class="position-relative">
-                <a href="javascript:void(0)">Blog <i class="fa-solid fa-angle-down"></i></a>
-                <ul class="submenu_wrapper">
-                  <li><a href="blog-default.php">Blog</a></li>
-                  <li><a href="blog-details.php">Blog Details</a></li>
-                  <li><a href="blog-details-left-sidebar.php">Blog Leftsidebar</a></li>
-                  <li><a href="blog-details-right-sidebar.php">Blog rightsidebar</a></li>
-                  <li><a href="blog-style1.php">Blog style 1</a></li>
-                  <li><a href="blog-style2.php">Blog style 2</a></li>
                 </ul>
-              </li>
+              </li> 
               <li><a href="contact.php">Contact</a></li>
               <li><a href="#">
                 
-                <button type="button" class="btn " data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" class="btn p-0 fw-semibold "  data-bs-toggle="modal" data-bs-target="#exampleModal">
                Register
                 </button>
               </a></li>
             </ul>
           </div>
-          <div class="col-xl-2 col-lg-2  d-none d-xxl-block d-xl-block">
-            <div class="header_search_menu d-flex">
-              <div class="dropdown dropdown_search">
-                <button class="search-btn " data-bs-toggle="dropdown" aria-expanded="true"><i
-                    class="fa-solid fa-magnifying-glass"></i></button>
-                <div class="dropdown-menu dropdown-menu-end" data-popper-placement="bottom-end">
-                  <form class="search-form d-flex align-items-center gap-2">
-                    <input type="text" placeholder="Search..." class="theme-input bg-transparent">
-                    <button type="submit" class="submit-btn">Go</button>
-                  </form>
-                </div>
-              </div>
-
-
-              <!-- right offcanvas -->
-              <div class="offcanvas_right">
-                <button class="header_toggle_btn bg-transparent offcanvas_btn" type="button" data-bs-toggle="offcanvas"
-                  data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
-
+      
           <!-- mobile menu bar -->
           <div class="col-lg-10 col-md-8 col-6 d-block d-xxl-none d-xl-none">
             <div class="d-flex align-items-center gap-2 justify-content-end">
